1d78e0894bd1c7223c50922cfe5c6605f7f270e7
MarcoRhayden <rhayden@arkansoftware.com>
2025-09-13 02:59:42 -0300

    refactor(app:ports): update IConfigureProvider

--- src/application/ports/IConfigProvider.hpp
+++ src/application/ports/IConfigProvider.hpp
@@ -4,9 +4,9 @@
 
 namespace arkan::relay::application::ports {
 
-struct IConfigProvider {
-    virtual ~IConfigProvider() = default;
-    virtual arkan::relay::domain::Settings load_or_create(const std::string& path) = 0;
-};
+    struct IConfigProvider {
+        virtual ~IConfigProvider() = default;
+        virtual arkan::relay::domain::Settings load_or_create(const std::string& path) = 0;
+    };
 
 } // namespace arkan::relay::application::ports
